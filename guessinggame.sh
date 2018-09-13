9061dfe24be4691ca09a52664e34921f69da80c0
mmncoder <mamnrashid@gmail.com>
2018-09-13 19:32:28 +0300

    guessinggame.sh has been modfied according to the requirment of the assignment

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,48 +1,33 @@
 #!/bin/bash
+
+#read the total file number of the current direcitory
+fileNumber=`ls |wc -l`
+
 function playGame()
 {
-	echo "Great! Now let's play the game..."
+	echo "Welcome to the Guessing Game! In this game you have to guess how many files in the current directory."
 	echo "Please enter your guess (guess must be a postive ineger):"
 
 	#read user input
 	read guess
+
 	#evalutating userGuess
-	while [[ $guess -ne 3 ]]
+	while [[ $guess -ne $fileNumber ]]
 		do
-			if [[ $guess -gt 3 ]]
+			if [[ $guess -gt $fileNumber ]]
 			then
 				echo "Your guess is to high."
-			elif [[ $guess -lt 3 ]]
+			elif [[ $guess -lt $fileNumber ]]
 			then
 				echo "Sorry your guess is too low."
 			fi
 
-			echo "Please enter your guess again (remember guess must be a postive integer" 
+			echo "Please enter your guess again (remember guess must be a postive integer):" 
 			read guess
 		done
 	#upon successful guess print congratulation message
 	echo "Congratulation! Your guees is CORRECT!!!"
 }
 
-#function prompt defining
-function prompt()
-{
-	echo "Hello,  Welcome to the Guessing Game! In this game you have to guess how many files in the current directory."
-	echo "Are you ready to play the game?"
-	echo "If yes, then type 'Y/y' and press EENTER; If no then type any other key expect Y/y and press ENTER" 
-
-	#read user input
-	read answer
-
-	if [[ "$answer" = "Y"  || "$answer" = "y" ]]
-	then
-		#call the playGame method to play the game
-		playGame
-	else
-		#if user decline to play, exit from the game with a exiting message
-		echo "You've declined to play the game. Exiting from the game."
-		exit
-	fi
-}
 #prompt user for playing the game
-prompt
+playGame
